e88b475a6054eabb0c3b36141ebeea0a5b439aa7
Hikaru Suido <hikaru.suido@gmail.com>
2017-11-27 07:08:51 +0900

    Complete reservation system

--- Reservation.java
+++ Reservation.java
@@ -9,14 +9,23 @@ public class Reservation{
     }
   }
 
-  boolean reserve(int id, int num){
-    for(int i=0;i<seat.length;i++){
-      for(int j=0;j<seat[i].length;j++){
-        if(seat[i][j]==-1){
-          for(int k=j; k<j+num; k++){
-            if(seat[i][k]==-1){
+  synchronized boolean reserve(int id, int num){
+    int sum = 0;
+
+    for(int i=0; i<seat.length; i++){
+      for(int j=0; j<seat[i].length; j++){
+        if(seat[i].length-j>=num){
+          if(seat[i][j]==-1){
+            for(int k=j; k<j+num; k++){
+              sum += seat[i][k];
             }
-            seat[i][k]=id;
+            if(sum == -num){
+              for(int l=j; l<j+num; l++){
+                seat[i][l] = id;
+                System.out.println("ID:"+id+"  ["+i+","+l+"]");
+              }
+              return true;
+            }
           }
         }
       }
@@ -32,9 +41,10 @@ public class Reservation{
       System.out.println();
     }
   }
+
   public static void main(String args[]){
-    int thread_num = 5;      //予約を取りに来る顧客(窓口)数
-    Reservation rs = new Reservation(10,10); //6,15は座席数
+    int thread_num = 5;
+    Reservation rs = new Reservation(6,15);
     Passengers ps[] = new Passengers[thread_num];
     for(int i=0;i<thread_num;i++){
       ps[i] = new Passengers(i,rs);
@@ -49,7 +59,6 @@ public class Reservation{
   }
 }
 
-
 class Passengers extends Thread{
   int id;
   Reservation rs;
@@ -60,10 +69,12 @@ class Passengers extends Thread{
   }
 
   public void run(){
-    for(int i=0;i<10;i++){ //10回行う
+    for(int i=0;i<10;i++){
       int num = (int)(Math.random()*6+1);
       if(rs.reserve(id, num)){
         System.out.println("ID:"+id+"  reserved "+num+" seats.");
+      }else{
+        System.out.println("FAILURE  ID:"+id+"  NUMBER:"+num);
       }
     }
   }
